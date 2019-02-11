67360a2efca0d71ff7617b6b4fd22533f44e14f2
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-02-11 15:34:30 -0600

    Scripts

--- scripts/mffe.sh
+++ scripts/mffe.sh
@@ -6,17 +6,22 @@
 #    Registration to atlas
 #    Extract cross-sectional area
 
+# For X11 forwarding when running within docker container
+export DISPLAY=host.docker.internal:0
+
 # Compute T1W from multiecho mFFE
 # Register the echoes
 # Compute the computation
 
 # Spinal cord
-#sct_propseg
-sct_deepseg_sc -i ../INPUTS/mffe.nii.gz -c t2 -ofolder OUTPUTS -qc OUTPUTS/qc_deepseg_sc
+sct_propseg -i ../INPUTS/mffe.nii.gz -c t2 -ofolder OUTPUTS -CSF -cross -centerline-binary
+
+#-qc OUTPUTS/qc_deepseg_sc
+sct_deepseg_sc -i ../INPUTS/mffe.nii.gz -c t2 -ofolder OUTPUTS 
 
 # Gray matter
 #sct_segment_graymatter
-sct_deepseg_gm
+sct_deepseg_gm -i INPUTS/mffe.nii.gz -o OUTPUTS/mffe_gm.nii.gz
 
 # White matter = cord - gray matter
 sct_maths
