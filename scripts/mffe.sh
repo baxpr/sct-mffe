d364b40013493a65bfb1fd46979f8374aa5bd9a2
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-02-11 17:32:39 -0600

    Script

--- scripts/mffe.sh
+++ scripts/mffe.sh
@@ -9,19 +9,45 @@
 # For X11 forwarding when running within docker container
 export DISPLAY=host.docker.internal:0
 
-# Compute T1W from multiecho mFFE
-# Register the echoes
-# Compute the computation
+# Working dir
+cd ../OUTPUTS
+
+# Average echoes from multiecho mFFE
+# Poor result from segmentation in this particular case
+#sct_maths -i mffe_e1.nii.gz -add mffe_e2.nii.gz,mffe_e3.nii.gz -o ../OUTPUTS/mffe_sum.nii.gz
 
 # Spinal cord
-sct_propseg -i ../INPUTS/mffe.nii.gz -c t2 -ofolder OUTPUTS -CSF -cross -centerline-binary
+#sct_propseg -i ../INPUTS/mffe.nii.gz -c t2 -ofolder OUTPUTS -CSF -cross -centerline-binary
 
 #-qc OUTPUTS/qc_deepseg_sc
-sct_deepseg_sc -i ../INPUTS/mffe.nii.gz -c t2 -ofolder OUTPUTS 
+#sct_deepseg_sc -i ../INPUTS/mffe_e1.nii.gz -c t2
 
 # Gray matter
 #sct_segment_graymatter
-sct_deepseg_gm -i INPUTS/mffe.nii.gz -o OUTPUTS/mffe_gm.nii.gz
+#sct_deepseg_gm -i ../INPUTS/mffe_e1.nii.gz -o mffe_e1_gm.nii.gz
 
 # White matter = cord - gray matter
-sct_maths
+
+
+# Segment cord
+sct_deepseg_sc -i ../INPUTS/mffe_e1.nii.gz -c t2
+
+# Create mask
+sct_create_mask -i ../INPUTS/mffe_e1.nii.gz -p centerline,mffe_e1_seg.nii.gz -size 40mm -o mffe_e1_mask.nii.gz
+
+# Crop data for faster processing
+sct_crop_image -i ../INPUTS/mffe_e1.nii.gz -m mffe_e1_mask.nii.gz -o mffe_e1_crop.nii.gz
+
+# Create label 4 at the mid-FOV, because we know the FOV is centered at C3-C4 disc.
+sct_label_utils -i mffe_e1_seg.nii.gz -create-seg -1,4 -o label_c3c4.nii.gz
+
+# Register template->T1w_ax (using template-T1w as initial transformation)
+# As it stands, this does not get the Z direction correct
+sct_register_to_template -i mffe_e1_crop.nii.gz -s mffe_e1_seg.nii.gz \
+-ldisc label_c3c4.nii.gz -ref subject -c t2 \
+-param step=1,type=seg,algo=slicereg,metric=MeanSquares,smooth=2:step=2,type=im,\
+algo=bsplinesyn,metric=MeanSquares,iter=5,gradStep=0.5
+
+# Warp template
+sct_warp_template -d mffe_e1_crop.nii.gz -w warp_template2anat.nii.gz
+
